e5fdc3d3ea4e2787bccdca6166e8ca831923893d
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-15 14:10:04 -0400

    add punctuation spec

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -8,7 +8,7 @@ describe "#encrypt" do
     let(:encrypted_string) { "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald" }
 
     it "encrypts a string" do
-      expect(encrypt(string, 3)).to eq encrypted_string
+      expect(encrypt(string, rotation)).to eq encrypted_string
     end
 
     context "with mixed case" do
@@ -16,7 +16,16 @@ describe "#encrypt" do
       let(:encrypted_mixed_case_string) { "The Quick Brown Fox jumped over the Lazy Dog" }
 
       it "preserves case" do
-        expect(encrypt(mixed_case_string, 3)).to eq encrypted_mixed_case_string
+        expect(encrypt(mixed_case_string, rotation)).to eq encrypted_mixed_case_string
+      end
+    end
+
+    context "with punctuation" do
+      let(:string_with_punctuation) { "...the quick brown fox - jumped over the lazy dog!" }
+      let(:encrypted_string_with_punctuation) { "...qeb nrfzh yoltk clu - grjmp lsbo qeb ixwv ald!" }
+
+      it "preserves punctuation" do
+        expect(encrypt(string_with_punctuation, rotation)).to eq encrypted_string_with_punctuation
       end
     end
   end
