ec2bf1e4c7c4d5fbb2808a26243a5185ff9c513d
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-15 14:28:58 -0400

    fixed completely wrong ciphering, rotation, added variable rotation spec

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -5,7 +5,8 @@ describe "#encrypt" do
   describe "basic encryption" do
     let(:string) { "the quick brown fox jumped over the lazy dog" }
     let(:rotation) { 3 }
-    let(:encrypted_string) { "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald" }
+    let(:encrypted_string) { "wkh txlfn eurzq ira mxpshg ryhu wkh odcb grj" }
+
 
     it "encrypts a string" do
       expect(encrypt(string, rotation)).to eq encrypted_string
@@ -13,7 +14,7 @@ describe "#encrypt" do
 
     context "with mixed case" do
       let(:mixed_case_string) { "The Quick Brown Fox jumped over the Lazy Dog" }
-      let(:encrypted_mixed_case_string) { "The Quick Brown Fox jumped over the Lazy Dog" }
+      let(:encrypted_mixed_case_string) { "Wkh Txlfn Eurzq Ira mxpshg ryhu wkh Odcb Grj" }
 
       it "preserves case" do
         expect(encrypt(mixed_case_string, rotation)).to eq encrypted_mixed_case_string
@@ -22,11 +23,27 @@ describe "#encrypt" do
 
     context "with punctuation" do
       let(:string_with_punctuation) { "...the quick brown fox - jumped over the lazy dog!" }
-      let(:encrypted_string_with_punctuation) { "...qeb nrfzh yoltk clu - grjmp lsbo qeb ixwv ald!" }
+      let(:encrypted_string_with_punctuation) { "...wkh txlfn eurzq ira - mxpshg ryhu wkh odcb grj!" }
 
       it "preserves punctuation" do
         expect(encrypt(string_with_punctuation, rotation)).to eq encrypted_string_with_punctuation
       end
     end
+
+    context "with variable rotation" do
+      let(:short_rotation) { 1 }
+      let(:encrypted_string_short) { "uif rvjdl cspxo gpy kvnqfe pwfs uif mbaz eph" }
+      let(:long_rotation) { 13 }
+      let(:encrypted_string_long) { "gur dhvpx oebja sbk whzcrq bire gur ynml qbt" }
+      let(:negative_rotation) { -1 }
+      let(:encrypted_string_negative) { "sgd pthbj aqnvm enw itlodc nudq sgd kzyx cnf" }
+
+      it "accepts a rotation vector" do
+        expect(encrypt(string, short_rotation)).to eq encrypted_string_short
+        expect(encrypt(string, long_rotation)).to eq encrypted_string_long
+        expect(encrypt(string, negative_rotation)).to eq encrypted_string_negative
+      end
+    end
+
   end
 end
