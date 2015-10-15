5f44ccf43e51a6684986a1df4d26ab6d360fb36b
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-15 14:06:11 -0400

    add mixed case spec

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 require_relative "../caesar.rb"
 
 describe "#encrypt" do
-  let(:string) { "the quick brown fox jumped over the lazy dog"}
-  let(:encrypted_string) { "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald" }
+  describe "basic encryption" do
+    let(:string) { "the quick brown fox jumped over the lazy dog" }
+    let(:rotation) { 3 }
+    let(:encrypted_string) { "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald" }
 
-  it "encrypts a string" do
-    expect(encrypt(string)).to eq encrypted_string
+    it "encrypts a string" do
+      expect(encrypt(string, 3)).to eq encrypted_string
+    end
+
+    context "with mixed case" do
+      let(:mixed_case_string) { "The Quick Brown Fox jumped over the Lazy Dog" }
+      let(:encrypted_mixed_case_string) { "The Quick Brown Fox jumped over the Lazy Dog" }
+
+      it "preserves case" do
+        expect(encrypt(mixed_case_string, 3)).to eq encrypted_mixed_case_string
+      end
+    end
   end
 end
