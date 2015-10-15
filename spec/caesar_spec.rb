3e4fccca2d3f29bc0c6eb9d80953c3480e819e2a
Rebecca Paz <r.pazyaquian@gmail.com>
2015-10-15 13:57:25 -0400

    describe encrypt method, input, expected output

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
 require_relative "../caesar.rb"
+
+describe "#encrypt" do
+  let(:string) { "the quick brown fox jumped over the lazy dog"}
+  let(:encrypted_string) { "qeb nrfzh yoltk clu grjmp lsbo qeb ixwv ald" }
+
+  it "encrypts a string" do
+    
+  end
+end
